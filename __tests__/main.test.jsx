96a26bf174cc1294a1239f898545d6889ce24b29
Boyer laurent <154669613+LaurentBoyer1976@users.noreply.github.com>
2025-03-08 14:55:09 +0100

    Réorganisation des fichiers et mise à jour des chemins d'importation, suppression des fichiers CSS et JSX obsolètes, ajout de nouveaux fichiers dans les répertoires appropriés.

--- __tests__/main.test.jsx
+++ __tests__/main.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom'; // Modifiez cette ligne
-import App from '../src/App';
+import App from '../src/Js/App';
 
 test('renders Vite + React heading', () => {
   render(<App />);
