d5c5c4ff39d7331eb00a241e2f272d9e2707e3cd
Boyer laurent <154669613+LaurentBoyer1976@users.noreply.github.com>
2025-03-14 10:40:07 +0100

    Ajout d'icônes de navigation pour le composant SlidingPicture et suppression de l'image de couverture du tableau des images.

--- src/Js/Components/SlidingPicture.jsx
+++ src/Js/Components/SlidingPicture.jsx
@@ -1,9 +1,28 @@
 import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
+
+const LeftArrow = () => {
+    return (
+        <FontAwesomeIcon
+            icon={faChevronLeft}
+            className="slidingPicture__container--previous-icon"        
+        />
+    );
+}
+const RightArrow = () => {
+    return (
+        <FontAwesomeIcon
+            icon={faChevronRight}
+            className="slidingPicture__container--next-icon"
+        />
+    );
+}
 
 const SlidingPicture = ({ logement }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const gallery = [logement.cover, ...logement.pictures];
+    const gallery = logement.pictures;
     const totalPictures = gallery.length;
 
     const handleNext = () => {
@@ -16,9 +35,9 @@ const SlidingPicture = ({ logement }) => {
     return (
         <div type='slideshow' className='slidingPicture'>
             <div className='slidingPicture__container'>
-                <span className='slidingPicture__container--previous' onClick={handlePrevious}></span>
+                <span className='slidingPicture__container--previous' onClick={handlePrevious}><LeftArrow /></span>
                 <img src={gallery[currentIndex]} alt={logement.title} className="slidingPicture__container--img" />            
-                <span className='slidingPicture__container--next' onClick={handleNext}></span>
+                <span className='slidingPicture__container--next' onClick={handleNext}><RightArrow /></span>
             </div>
             { totalPictures <= 1 ? null : <span className='picture__counter'>{currentIndex+1}/{totalPictures}</span>}            
         </div>
